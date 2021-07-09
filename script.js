df04b11aef4121b3ec08eb1ec87b0d8878fedf83
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-07-09 14:19:47 -0700

    updated formatting of table

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 // global variables for each input
+var saveBtn = document.querySelector('.saveBtn');
 var oneInput = document.getElementById('input-field-1')
 
 // set today's date on the top of the scheduler
-function todaysDate() {
-var today = new Date();
-var intDate = string(today.getDate());
-var intDay = string(today.getDay());
-var intMonth = string(today.getMonth());
-var intYear = string(today.getYear());
-var intHours = string(today.getHours());
-var intMinutes = string(today.getMinutes());
-var intSeconds = string(today.getSeconds());
-var time = intDate + " " + intDay + " " + intMonth + " " + intYear + " " + intHours + " " + intMinutes + " " + intSeconds;
-document.getElementById("currentDay").innerHTML = time;
-console.log(time);
-}
+// moment not working
+var todayDate = moment().format('dddd,MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
-// set colors for time blocks depending on current time
-// green = future
-// red = present
-// grey = past
+// set colors for time blocks depending on current time if/else statement
+// green = .future
+// red = .present
+// grey = .past
 
 
 // save data buttons 
+saveBtn.addEventListener("click", saveData);
+
 function saveData() {
     localStorage.setItem("input", oneInput.value);
 }
-
+ 
 // retrieve stored data
 
 function retrieveData() {
-
+    localStorage.getItem("input");
 }
+
+// $("oneInput .description").val(localStorage(get.item("oneInput")));
